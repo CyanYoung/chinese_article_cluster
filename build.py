14b0339d53acb182be4f6a4707abd53b4f9410ee
yangjc <bupt_yjc@163.com>
2018-10-30 20:15:00 +0800

    add featurize

--- build.py
+++ build.py
@@ -23,14 +23,18 @@ paths = {'mean_lsi': 'model/mean_lsi.pkl',
          'mean_lda': 'model/mean_lda.pkl'}
 
 
+def featurize(tfidf_docs, feat):
+    topic_docs = list()
+    for doc in feat[tfidf_docs]:
+        topic_docs.append([score for ind, score in doc])
+    return np.array(topic_docs)
+
+
 def fit(path):
     with open(path, 'rb') as f:
         tfidf_docs = pk.load(f)
     for name, feat in feats.items():
-        topic_docs = list()
-        for doc in feat[tfidf_docs]:
-            topic_docs.append([score for ind, score in doc])
-        topic_docs = np.array(topic_docs)
+        topic_docs = featurize(tfidf_docs, feat)
         model = KMeans(n_clusters=topic_num, n_init=10, max_iter=100)
         model.fit(topic_docs)
         with open(map_item('mean_' + name, paths), 'wb') as f:
