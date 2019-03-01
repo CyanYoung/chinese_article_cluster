8b037ccf48c0b54dbfb52ff3785afacd7d86aa8f
yangjc <bupt_yjc@163.com>
2019-03-01 20:52:54 +0800

    simplify format

--- eval.py
+++ eval.py
@@ -2,22 +2,14 @@ import pickle as pk
 
 import numpy as np
 
+from cluster import models
+
 from util import map_item
 
 
 path_test = 'feat/sent_test.pkl'
 with open(path_test, 'rb') as f:
     sents = pk.load(f)
-
-path_lsi = 'model/lsi.pkl'
-path_lda = 'model/lda.pkl'
-with open(path_lsi, 'rb') as f:
-    lsi = pk.load(f)
-with open(path_lda, 'rb') as f:
-    lda = pk.load(f)
-
-models = {'lsi': lsi,
-          'lda': lda}
 
 
 def test(name, sents):
