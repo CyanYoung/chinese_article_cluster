85259f326951ade82117a4e64a4efb143fcd809a
yangjc <bupt_yjc@163.com>
2019-03-01 20:50:31 +0800

    change to perp

--- eval.py
+++ eval.py
@@ -1,82 +1,30 @@
-import json
 import pickle as pk
 
 import numpy as np
 
-from cluster import featurize
-
-from util import flat_read, map_item
+from util import map_item
 
 
-path_topic = 'data/topic.json'
-with open(path_topic, 'r') as f:
-    topics = json.load(f)
-
-topic_num = len(topics)
-
-path_data = 'data/test.csv'
 path_test = 'feat/sent_test.pkl'
-labels = flat_read(path_data, 'label')
 with open(path_test, 'rb') as f:
-    tfidf_docs = pk.load(f)
+    sents = pk.load(f)
 
 path_lsi = 'model/lsi.pkl'
 path_lda = 'model/lda.pkl'
-path_km_lsi = 'model/km_lsi.pkl'
-path_km_lda = 'model/km_lda.pkl'
 with open(path_lsi, 'rb') as f:
     lsi = pk.load(f)
 with open(path_lda, 'rb') as f:
     lda = pk.load(f)
-with open(path_km_lsi, 'rb') as f:
-    km_lsi = pk.load(f)
-with open(path_km_lda, 'rb') as f:
-    km_lda = pk.load(f)
 
-feats = {'lsi': lsi,
-         'lda': lda}
-
-models = {'km_lsi': km_lsi,
-          'km_lda': km_lda}
+models = {'lsi': lsi,
+          'lda': lda}
 
 
-def label2ind(labels):
-    labels = sorted(list(set(labels)))
-    label_inds = dict()
-    for i in range(len(labels)):
-        label_inds[labels[i]] = i
-    return label_inds
-
-
-def ind2label(label_inds):
-    ind_labels = dict()
-    for label, ind in label_inds.items():
-        ind_labels[ind] = label
-    return ind_labels
-
-
-def test(tfidf_docs, labels):
-    label_inds = label2ind(labels)
-    inds = np.array([label_inds[label] for label in labels])
-    ind_labels = ind2label(label_inds)
-    for name, feat in feats.items():
-        model = map_item('km_' + name, models)
-        topic_docs = featurize(tfidf_docs, feat)
-        preds = model.predict(topic_docs)
-        max_labels = list()
-        rates = list()
-        for i in range(topic_num):
-            pred_args = np.where(preds == i)
-            match_inds = inds[pred_args]
-            counts = np.bincount(match_inds)
-            max_labels.append(ind_labels[np.argmax(counts)])
-            rates.append(max(counts) / sum(counts))
-        print('\n%s %s %.2f\n' % (name, 'acc:', sum(rates) / len(rates)))
-        formats = list()
-        for label, rate in zip(max_labels, rates):
-            formats.append('{} {:.2f}'.format(label, rate))
-        print(', '.join(formats))
+def test(name, sents):
+    model = map_item(name, models)
+    log = model.log_perplexity(sents)
+    print('\n%s %s %.2f' % (name, 'perp:', np.power(2, -log)))
 
 
 if __name__ == '__main__':
-    test(tfidf_docs, labels)
+    test('lda', sents)
